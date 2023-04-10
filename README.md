62db2b953c333c0e67fa7fa19f4b8b1cdc7c7204
Andrew Mou <andrew.mou99@gmail.com>
2023-04-10 11:50:07 -0500

    added additional comments

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# unit_conversion
+## About
+
+This tool provides functionality for converting between units of length and mass and adding or subtracting quantities
+of different (but compatible) units.
+
+## Dependencies
+
+This tool depends on node.js.
+
+## Installation
+
+In the command line, run the following in the desired directory:
+
+### `git clone https://github.com/0xluciela/unit_conversion.git`
+
+In the project directory, run:
+
+### `node app.js`
+
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+## Usage
+
+To convert between compatible units, use the dropdown to set the units to convert to and from and enter the numerical value
+corresponding to the first unit in the first field. Press the "Compute" button to populate the second field.
+
+To add quantities in potentially differing units, set the units and the corresponding numerical values. The sum should
+be displayed in both units below the fields after pressing "Compute."
+
+## Assumptions
+
+This tool was developed on Arch Linux.
+
+
--- app.js
+++ app.js
@@ -1,24 +1,27 @@
+// Import functions and constants
 let conversion = require('./conversion.js');
 const { digit_precision, results, length_conversions, mass_conversions, unit_type_to_conversion_map } = require('./conversion.js');
 let express = require('express');
 
+// Launch express server
 let app = express();
 app.use(express.static('public'));
 
+// Default conversion case
 app.get('/convert/default/value/:value/old_unit/:old_unit/new_unit/:new_unit/unit_type/:unit_type', (req, res) => {
-
     let value = parseFloat(req.params.value);
     let old_unit = req.params.old_unit;
     let new_unit = req.params.new_unit;
     let unit_type = req.params.unit_type;
 
+    // Call do_conversion() based on parameters passed from http request
     let output = conversion.do_conversion(value, old_unit, new_unit, unit_type_to_conversion_map[unit_type]);
     output = conversion.round(output);
     res.send(`${output}`);
 });
-  
+
+// Addition/subtraction case
 app.get('/convert/addsub/operation/:operation/value1/:value1/value2/:value2/unit1/:unit1/unit2/:unit2/unit_type/:unit_type', (req, res) => {
-    
     let operation = req.params.operation;
     let value1 = parseFloat(req.params.value1);
     let value2 = parseFloat(req.params.value2);
@@ -26,17 +29,21 @@ app.get('/convert/addsub/operation/:operation/value1/:value1/value2/:value2/unit
     let unit2 = req.params.unit2;
     let unit_type = req.params.unit_type;
 
+    // Subtraction is equivalent to adding the additive inverse
     if (operation === "-") { value2 *= -1; }
 
+    // Call add_values() based on parameters passed from http request
     let [output1, output2] = conversion.add_values(value1, value2, unit1, unit2, unit_type_to_conversion_map[unit_type]);
     [output1, output2] = conversion.round(output1, output2);
 
+    // Reset value2 in the subtraction case for easy output
     if (operation === "-") { value2 *= -1; }
 
     // '&' is used to separate the strings
     res.send(`${value1} ${unit1} ${operation} ${value2} ${unit2} is equal to ${output1} ${unit1}.&${value1} ${unit1} ${operation} ${value2} ${unit2} is equal to ${output2} ${unit2}.`);
 });
-  
+
+// Listen for connections
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
